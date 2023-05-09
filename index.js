e3ff1ac58cf42c6f262a04be8a1c9dd78f0eb5df
Johnny Proano <jproano22@yahoo.com>
2023-05-09 13:13:13 -0400

    added handleWatchlistclick funtion and event listener to data-set, added temp arr to store movie details and push in to watchlist array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-console.log(uuidv4);
 let api = "";
+let watchlistArray = [];
 
 const movieList = document.getElementById("movie-list");
 const formSubmit = document.getElementById("form-submit");
@@ -9,7 +9,10 @@ let filmIcon = document.getElementById("film-icon");
 formSubmit.addEventListener("submit", getMovieBySearch);
 
 document.addEventListener("click", function (e) {
-  console.log(e.target.dataset);
+  if (e.target.dataset.watchlist) {
+    console.log("click event");
+    handleWatchlistClick(e.target.dataset);
+  }
 });
 
 function getMovieBySearch() {
@@ -20,7 +23,6 @@ function getMovieBySearch() {
   fetch(`http://www.omdbapi.com/?apikey=${api}&s=${searchBarValue.value}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.Response);
       mapMovies(data.Search);
       searchBarValue.value = "";
     });
@@ -36,7 +38,6 @@ function getMovieByTitle(data) {
   fetch(`http://www.omdbapi.com/?apikey=${api}&t=${data.Title}`)
     .then((response) => response.json())
     .then((movieData) => {
-      console.log(movieData);
       renderMovieHtml(movieData);
     });
 }
@@ -51,12 +52,26 @@ function renderMovieHtml(movieData) {
     <h2 class="title">${Title}</h2>
     <img class="poster" src=${Poster}>
     <div class="movie-detail1">
+    <div class="year-runtime-rating">
       <p class="year">${Year}</p>
       <p class="runtime">${Runtime}</p>
       <p class="ratings">⭐️ ${Ratings[0].Value}</p>
+    </div>
       <p class="genre">${Genre}</p>
       <p class="actors">${Actors}</p>
-      <p class="watchlist-btn" id="watchlist-btn" data-watchlist="">➕ Watchlist</p>
+      <p 
+        class="watchlist-btn" 
+        id="watchlist-btn" 
+        data-watchlist="${uuidv4()}"
+        data-title="${Title}"
+        data-poster="${Poster}"
+        data-year="${Year}"
+        data-runtime="${Runtime}"
+        data-ratings="${Ratings[0].Value}"
+        data-actors="${Actors}"
+        data-plot="${Plot}"
+        "
+      >➕ Watchlist</p>
     </div>
     <p class="plot">${Plot}</p>
   </div>
@@ -66,6 +81,19 @@ function renderMovieHtml(movieData) {
   }
 }
 
-function handleWatchlistClick() {
-  console.log("watch click");
+function handleWatchlistClick(movieData) {
+  let tempMovieDetailObj = {};
+  tempMovieDetailObj = {
+    id: `${movieData.watchlist}`,
+    title: `${movieData.title}`,
+    poster: `${movieData.poster}`,
+    year: `${movieData.year}`,
+    runtime: `${movieData.runtime}`,
+    ratings: `${movieData.ratings}`,
+    actors: `${movieData.actors}`,
+    plot: `${movieData.plot}`,
+  };
+
+  watchlistArray.push(tempMovieDetailObj);
+  console.log(watchlistArray);
 }
